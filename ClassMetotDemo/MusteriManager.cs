182778f7cef8844d2d013966f0ffb508a2499c1a
Mu Tu <hadvidub@gmail.com>
2021-01-20 19:48:37 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            
+            Console.WriteLine("{0} nolu Id'li {1} {2} sisteme eklenmiştir",musteri.Id,musteri.Ad,musteri.Soyad);
+
+        }
+
+        public void Listele(Musteri[] musteri)
+        {
+            Console.WriteLine("Müşterilerin listesi");
+
+            foreach (var mus in musteri)
+            {
+                Console.WriteLine("Müşteri ID: {0}",mus.Id);
+                Console.WriteLine("Müşteri TC no: {0}",mus.TcNo);
+                Console.WriteLine("Müşteri Adı,Soyadı: {0} {1}",mus.Ad,mus.Soyad);
+                Console.WriteLine("Müşteri Telefon: {0}", mus.Telefon);
+                Console.WriteLine("Müşteri e-mail adresi: {0}",mus.mail);
+                Console.WriteLine("********************************");
+
+            }
+
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("{0} nolu Id'li {1} {2} sistemden silinmiştir.", musteri.Id, musteri.Ad, musteri.Soyad);
+
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MusteriManager musteriManager = new MusteriManager();
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.TcNo = "1111111111";
+            musteri1.Ad = "Ali";
+            musteri1.Soyad = "Ali";
+            musteri1.Telefon = "0537777777";
+            musteri1.mail = "aaa@aaa.com";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.TcNo = "22222222";
+            musteri2.Ad = "Veli";
+            musteri2.Soyad = "Veli";
+            musteri2.Telefon = "0537777777";
+            musteri2.mail = "aaa@aaa.com";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 3;
+            musteri3.TcNo = "33333333";
+            musteri3.Ad = "Ahmet";
+            musteri3.Soyad = "Ahmet";
+            musteri3.Telefon = "0537777777";
+            musteri3.mail = "aaa@aaa.com";
+
+            Musteri[] musteriler = new Musteri[] {musteri1, musteri2, musteri3 };
+
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+            musteriManager.Ekle(musteri3);
+            Console.WriteLine("----------------------------");
+
+            musteriManager.Listele(musteriler);
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("----------------------------");
+            musteriManager.Sil(musteri3);
+        }
+    }
+}
